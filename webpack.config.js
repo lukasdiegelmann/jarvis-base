1a0e31f6485ce94c67b8e846b3f3147ba995b701
EmptyOa <o2303d40144a30130c402140@gmail.com>
2020-12-16 00:31:36 +0100

    Structure Establishment

---
+++ webpack.config.js
@@ -0,0 +1,73 @@
+const PATH = require("path");
+
+module.exports = [
+    {
+        name: "npm.workspace/development",
+        mode: "development",
+        devtool: "source-map",
+        entry: {
+            "npm-workspace": PATH.resolve(__dirname, "./src/main.ts"),
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.ts$/,
+                    use: [
+                        {
+                            loader: "babel-loader",
+                            options: {
+                                presets: ["@babel/preset-env", "@babel/preset-typescript"],
+                                plugins: [
+                                    "@babel/plugin-transform-typescript",
+                                    "@babel/plugin-transform-runtime",
+                                ],
+                            },
+                        },
+                    ],
+                },
+            ],
+        },
+        resolve: {
+            extensions: [".ts"],
+        },
+        externals: [require("webpack-node-externals")()],
+        output: {
+            filename: "[name].bundle.js",
+            path: PATH.resolve(__dirname, "./dist/development"),
+        },
+    },
+    {
+        name: "npm.workspace/production",
+        mode: "production",
+        entry: {
+            "npm-workspace": PATH.resolve(__dirname, "./src/main.ts"),
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.ts$/,
+                    use: [
+                        {
+                            loader: "babel-loader",
+                            options: {
+                                presets: ["@babel/preset-env", "@babel/preset-typescript"],
+                                plugins: [
+                                    "@babel/plugin-transform-typescript",
+                                    "@babel/plugin-transform-runtime",
+                                ],
+                            },
+                        },
+                    ],
+                },
+            ],
+        },
+        resolve: {
+            extensions: [".ts"],
+        },
+        externals: [require("webpack-node-externals")()],
+        output: {
+            filename: "[name].bundle.js",
+            path: PATH.resolve(__dirname, "./dist/production"),
+        },
+    },
+];
